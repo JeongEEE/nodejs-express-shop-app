e3e5c79262904f8fe037e3b09a62526e2c1c2fa2
JeongEEE <tbtbtb156@naver.com>
2024-01-05 12:06:12 +0900

    init

--- src/server.js
+++ src/server.js
@@ -10,6 +10,10 @@ const config = require('config')
 const serverConfig = config.get('server')
 const mainRouter = require('./routes/main.router')
 const usersRouter = require('./routes/users.router')
+const productsRouter = require('./routes/products.router')
+const cartRouter = require('./routes/cart.router')
+const adminCategoryRouter = require('./routes/admin-categories.router')
+const adminProductsRouter = require('./routes/admin-products.router')
 
 const cookieEncryptionKey = process.env.COOKIE_ENCRYPTION_KEY
 app.use(
@@ -58,6 +62,10 @@ app.use('/static', express.static(join(__dirname, 'public')))
 
 app.use('/', mainRouter)
 app.use('/auth', usersRouter)
+app.use('/admin/categories', adminCategoryRouter)
+app.use('/admin/products', adminProductsRouter)
+app.use('/products', productsRouter)
+app.use('/cart', cartRouter)
 
 const port = 4000
 app.listen(port, () => {
